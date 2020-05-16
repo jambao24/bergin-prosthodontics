0afd60e4af5f7c7fd458e9c4ef487e16de6addf4
Minh Lu <micksumm@isu.edu>
2020-05-16 12:14:48 -0700

    Added phone number and icon.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import './Navigation.css';
-import { AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu';
+import React from "react";
+import "./Navigation.css";
+import { AppBar, Toolbar, Typography, Button } from "@material-ui/core";
+import PhoneIcon from "@material-ui/icons/Phone";
 
 const Navigation = () => {
   return (
     <div className="Navigation_Root">
       <AppBar position="static">
         <Toolbar className="Navigation_Toolbar">
-          <IconButton
-            edge="start"
-            className="Navigation_Menu_Button"
-            color="inherit"
-            aria-label="open drawer">
-            <MenuIcon />
-          </IconButton>
           <Typography className="Navigation_Title" variant="h5" noWrap>
             iloveyoubitch
           </Typography>
+          <PhoneIcon className = "Navigation_Phone_Icon" fontSize="small"/>
+          <div className="Navigation_Phone_Number">
+            <Typography variant="h7" noWrap>
+              206-866-6466
+            </Typography>
+          </div>
           <Button variant="contained">
             Contact Us
           </Button>
