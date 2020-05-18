5094c34ff52d3a24f59fd0a25231868ca12fde75
Summer Mickelsen <micksumm@isu.edu>
2020-05-18 10:13:47 -0700

    Removed comments, and got rid of unnecessary material ui package.

--- src/Theme.js
+++ src/Theme.js
@@ -3,15 +3,11 @@ import { createMuiTheme } from '@material-ui/core/styles';
 const theme = createMuiTheme({
   palette: {
     primary: {
-      // light: will be calculated from palette.primary.main,
       main: '#67697C',
-      // main: '#F08080',
-      // dark: will be calculated from palette.primary.main , 
       contrastText: 'white'
     },
     secondary: {
       main: '#C6878F',
-      // dark: will be calculated from palette.secondary.main, main: '#F8AD9D',
       contrastText: 'white'
     }
   },
