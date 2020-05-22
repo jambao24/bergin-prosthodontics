2549064d04ec64b6981259090317dbf87acdc133
MinhLu12 <minhlu3@gmail.com>
2020-05-21 18:51:09 -0700

    Add draft of structure of contact component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from "react";
 import Navigation from "./components/Navigation/Navigation";
+import ContactInfo from "./components/ContactInfo/ContactInfo";
 
 export default function App() {
-  return <Navigation></Navigation>
+  return (
+    <>
+      <Navigation></Navigation>
+      <div style={{minHeight: "1000px"}}>
+      </div>
+      <ContactInfo></ContactInfo>
+    </>
+  )
 }
---
+++ src/components/ContactInfo/ContactInfo.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import "./ContactInfo.css"
+
+const ContactInfo = () => {
+  return (
+    <div className="ContactInfo">
+      <div className="ContactInfo_Column">
+        <p>Office Hours Bitches</p>
+        <p>Monday</p>
+        <p>Monday</p>
+        <p>Monday</p>
+        <p>Monday</p>
+      </div>
+      <div className="ContactInfo_Column">
+        <p>Location</p>
+        <p>Seattle</p>
+        <p>Seattle</p>
+        <p>Seattle</p>
+        <p>Seattle</p>
+      </div>
+      <div className="ContactInfo_Column">
+        <p>Map</p>
+      </div>
+    </div>
+  )
+}
+
+export default ContactInfo;
