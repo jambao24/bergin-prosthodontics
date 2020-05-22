458b04abc993152ea0abffa5967ebb479a93cdb5
MinhLu12 <minhlu3@gmail.com>
2020-05-21 20:44:42 -0700

    Tidy up CSS a bit

--- src/components/ContactInfo/ContactInfo.js
+++ src/components/ContactInfo/ContactInfo.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import {Grid, Box} from '@material-ui/core';
-import { ThemeProvider } from '@material-ui/styles';
-import Theme from "../../Theme";
+import './ContactInfo.css';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1,
+    display: "flex",
+    backgroundColor: "#67697C",
   },
   paper: {
     padding: theme.spacing(2),
@@ -19,39 +19,37 @@ const ContactInfo = () => {
   const classes = useStyles();
 
   return (
-    <ThemeProvider theme={Theme}>
-      <div className={classes.root}>
-        <Grid container spacing={3}>
-          <Grid item xs={4}>
-            <Box flexDirection="column" className={classes.paper}>
-              <p>Office hours</p>
-              <p>Monday 8:30 AM - 5 PM</p>
-              <p>Monday 8:30 AM - 5 PM</p>
-              <p>Monday 8:30 AM - 5 PM</p>
-              <p>Monday 8:30 AM - 5 PM</p>
-            </Box>
-          </Grid>
-          <Grid item xs={4}>
-            <Box className={classes.paper}>
-              <p>Office hours</p>
-              <p>Monday 8:30 AM - 5 PM</p>
-              <p>Monday 8:30 AM - 5 PM</p>
-              <p>Monday 8:30 AM - 5 PM</p>
-              <p>Monday 8:30 AM - 5 PM</p>
-            </Box>
-          </Grid>
-          <Grid item xs={4}>
-            <Box className={classes.paper}>
-              <p>Office hours</p>
-              <p>Monday 8:30 AM - 5 PM</p>
-              <p>Monday 8:30 AM - 5 PM</p>
-              <p>Monday 8:30 AM - 5 PM</p>
-              <p>Monday 8:30 AM - 5 PM</p>
-            </Box>
-          </Grid>
-        </Grid>
-      </div>
-    </ThemeProvider>
+    <Grid container className={classes.root}>
+      <Grid item xs={4} className="ContactInformation_Grid_Left">
+        <Box className={classes.paper}>
+          <p>Office hours</p>
+          <p>Monday 8:30 AM - 5 PM</p>
+          <p>Monday 8:30 AM - 5 PM</p>
+          <p>Monday 8:30 AM - 5 PM</p>
+          <p>Monday 8:30 AM - 5 PM</p>
+        </Box>
+      </Grid>
+
+      <Grid item xs={4}>
+        <Box className={classes.paper}>
+          <p>Office hours</p>
+          <p>Monday 8:30 AM - 5 PM</p>
+          <p>Monday 8:30 AM - 5 PM</p>
+          <p>Monday 8:30 AM - 5 PM</p>
+          <p>Monday 8:30 AM - 5 PM</p>
+        </Box>
+      </Grid>
+
+      <Grid item xs={4} className="ContactInformation_Grid_Right">
+        <Box className={classes.paper}>
+          <p>Office hours</p>
+          <p>Monday 8:30 AM - 5 PM</p>
+          <p>Monday 8:30 AM - 5 PM</p>
+          <p>Monday 8:30 AM - 5 PM</p>
+          <p>Monday 8:30 AM - 5 PM</p>
+        </Box>
+      </Grid>
+    </Grid>
   );
 }
 
