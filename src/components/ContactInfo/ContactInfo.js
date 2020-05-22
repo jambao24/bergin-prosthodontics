8035f0fc940652bce40db83856ec098ad0817d6c
MinhLu12 <minhlu3@gmail.com>
2020-05-21 19:31:40 -0700

    Improve CSS

--- src/components/ContactInfo/ContactInfo.js
+++ src/components/ContactInfo/ContactInfo.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import "./ContactInfo.css"
+import Divider from '@material-ui/core/Divider';
 
 const ContactInfo = () => {
   return (
     <div className="ContactInfo">
       <div className="ContactInfo_Column">
-        <p>Office Hours Bitches</p>
+        <p className="ContactInfo_Title">Office Hours Bitches</p>
         <p>Monday</p>
         <p>Monday</p>
         <p>Monday</p>
         <p>Monday</p>
       </div>
+      <Divider orientation="vertical" flexItem />
       <div className="ContactInfo_Column">
-        <p>Location</p>
+        <p className="ContactInfo_Title">Location</p>
         <p>Seattle</p>
         <p>Seattle</p>
         <p>Seattle</p>
         <p>Seattle</p>
       </div>
+      <Divider orientation="vertical" flexItem />
       <div className="ContactInfo_Column">
-        <p>Map</p>
+        <p className="ContactInfo_Title">Map</p>
       </div>
     </div>
   )
