aac22d46444e63c36e4db0f24fd683129f411013
MinhLu12 <minhlu3@gmail.com>
2020-06-20 13:29:32 -0700

    BP-23 (Standardize CSS Usage) (#11)
    * Download SCSS, fix project path for SCSS files, update ContactInfo and WIP for Navigation
    
    * Finish refactoring SCSS for Navigation and TabDropdown, remove Theme JS from Material UI
    
    * Address Summer's feedback to rename and use reusable SCSS

--- src/components/ContactInfo/ContactInfo.js
+++ src/components/ContactInfo/ContactInfo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import cn from './ContactInfo.module.css';
+import cn from './ContactInfo.module.scss';
 import classNames from 'classnames/bind';
 
 const ContactInfo = () => {
--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import theme from './Theme';
 import App from './App';
-
-import { ThemeProvider } from '@material-ui/core/styles';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
       <App />
-    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
