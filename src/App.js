bafd4e0c3f8627ac3fafed320197520ddfb49a8f
micksumm <65046453+micksumm@users.noreply.github.com>
2020-06-21 19:51:30 -0700

    Bp 7 build before and after component (#12)
    * Added design for the homepage.
    
    * Added Before and After photos and text.
    
    * Styled the before and after component.
    
    * Style before and after, add pathing and pictures out of public/
    
    * Changed spacing in Before and After Component.
    
    * Addressed Feedback, including style changes and font changes.
    
    Co-authored-by: Minh Lu <minhlu3@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Navigation from "./components/Navigation/Navigation";
 import ContactInfo from "./components/ContactInfo/ContactInfo";
+import BeforeAndAfter from "./components/BeforeAndAfter/BeforeAndAfter";
 
 export default function App() {
   return (
     <>
       <Navigation></Navigation>
-      <div style={{minHeight: "1000px"}}></div>
+      <div style={{minHeight: "507px"}}></div>
+      <BeforeAndAfter></BeforeAndAfter>
+      <div style={{minHeight: "400px"}}></div>
       <ContactInfo></ContactInfo>
     </>
   )
