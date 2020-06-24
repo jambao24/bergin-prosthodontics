684ccacc8320327817cca02d896f5bbe371407e0
micksumm <65046453+micksumm@users.noreply.github.com>
2020-06-23 20:47:17 -0700

    Bp 6 build featured services component (#13)
    * Added design for the homepage.
    
    * Added Service photos
    
    * Implemented featured services component.
    
    * Resized the service components, added global text size.

---
+++ src/components/FeaturedServices/FeaturedServices.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import cn from './FeaturedServices.module.scss';
+import CosmeticDentistry from 'assets/services/cosmetic_dentistry.png';
+import ImplantRestoration from 'assets/services/implant_restoration.png';
+import RemovableProstho from 'assets/services/removable_prosthodontics.png';
+import RestorativeDentistry from 'assets/services/restorative_dentistry.png';
+
+
+const FeaturedServices = () => {
+    return (
+        <>
+            <div className={cn.FeaturedServices}>
+                <div className={cn.Text}>
+                    <span className={cn.Header}>FEATURED SERVICES</span>
+                    <hr className={cn.Break}></hr>
+                    <span className={cn.Caption}>Handcrafted artistry meets cutting-edge dentistry</span>
+                </div>
+
+                <div className={cn.Images}>
+                    <img src={RestorativeDentistry}></img>
+                    <img src={RemovableProstho}></img>
+                    <img src={CosmeticDentistry}></img>
+                    <img src={ImplantRestoration}></img>
+                </div>
+            </div>
+        </>
+    ) 
+}
+
+export default FeaturedServices
