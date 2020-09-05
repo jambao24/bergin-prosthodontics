409a2377112f3cc81cb48bf5efaf38413154c982
Minh Lu <minhlu3@gmail.com>
2020-09-05 13:27:56 -0700

    Fix remaining spacing issues, make map smaller to align

--- src/components/ContactInfo/ContactInfo.js
+++ src/components/ContactInfo/ContactInfo.js
@@ -35,6 +35,11 @@ const ContactInfo = () => {
                         ></iframe>
                     </div>
                 </div>
+
+                <div className={cn.Copyright}>
+                    © 2020 Junping Bergin PLLC, Incorporated. All Rights
+                    Reserved.
+                </div>
             </div>
         </>
     );
--- src/pages/contact/ContactUs.js
+++ src/pages/contact/ContactUs.js
@@ -39,7 +39,7 @@ const ContactUs = () => {
                         <iframe
                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2687.2076114399465!2d-122.2885698841777!3d47.660963092213805!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5490149cab95f4f7%3A0xfbe3c5ac7551bbd7!2sBergin%20Prosthodontics!5e0!3m2!1sen!2sus!4v1590447131151!5m2!1sen!2sus"
                             width="600"
-                            height="450"
+                            height="400"
                             frameborder="0"
                             style={{ border: 0 }}
                             allowfullscreen="true"
