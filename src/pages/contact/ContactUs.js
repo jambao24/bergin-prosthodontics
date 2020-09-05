56c343eebaca3d05f1014f4b04804c8c579c3570
Minh Lu <minhlu3@gmail.com>
2020-09-05 13:16:42 -0700

    Remove unneeded SCSS, fix text sizes

--- src/pages/contact/ContactUs.js
+++ src/pages/contact/ContactUs.js
@@ -1,7 +1,6 @@
 import React from "react";
 import cn from "./ContactUs.module.scss";
 import classNames from "classnames/bind";
-/*import classNamesBind from "classnames/bind";*/
 
 const ContactUs = () => {
     return (
@@ -10,7 +9,9 @@ const ContactUs = () => {
                 <div className={classNames(cn.Column, cn.Column_Left)}>
                     <div>
                         <p className={cn.BigHeader}>Contact Us</p>
-                        <p className={cn.BigText}>We'd love to hear from you!</p>
+                        <p className={cn.BigText}>
+                            We'd love to hear from you!
+                        </p>
                         <br></br>
                         <br></br>
                         <p className={cn.Bold_Header}>Email</p>
@@ -33,18 +34,22 @@ const ContactUs = () => {
                     </div>
                 </div>
 
-
                 <div className={classNames(cn.Column, cn.Column_Right)}>
                     <div>
                         <iframe
                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2687.2076114399465!2d-122.2885698841777!3d47.660963092213805!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5490149cab95f4f7%3A0xfbe3c5ac7551bbd7!2sBergin%20Prosthodontics!5e0!3m2!1sen!2sus!4v1590447131151!5m2!1sen!2sus"
-                            width="600" height="450" frameborder="0" style={{ border: 0 }} allowfullscreen="true" aria-hidden="false" tabindex="1"
+                            width="600"
+                            height="450"
+                            frameborder="0"
+                            style={{ border: 0 }}
+                            allowfullscreen="true"
+                            aria-hidden="false"
+                            tabindex="1"
                             title="Bergin Prosthodontics Location"
                         ></iframe>
                     </div>
                 </div>
             </div>
-
         </>
     );
 };
